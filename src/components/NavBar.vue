6e423d0c6ee0d26688b40914415d2f3a4cb5406c
Yavor Spassov <yavor@hph.io>
2023-07-24 23:53:29 +0300

    Changing the default Flowbite logo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,13 +3,9 @@
     <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" class="flex items-center">
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            class="h-8 mr-3"
-            alt="Flowbite Logo"
-          />
+          <img src="@/assets/Music.png" class="h-8 mr-3" alt="Music app Logo" />
           <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
-            >Flowbite</span
+            >Music app</span
           >
         </a>
         <button
