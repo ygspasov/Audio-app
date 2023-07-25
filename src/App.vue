bea451ee3c7a2863b71a94784bf7c83e876fe4bc
Yavor Spassov <yavor@hph.io>
2023-07-25 03:00:07 +0300

    Adding Hero section

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
-  <Navbar></Navbar>
+  <div class="mx-4">
+    <Navbar></Navbar>
+    <HeroSection />
+  </div>
 </template>
 
 <script>
 import Navbar from "./components/NavBar.vue";
+import HeroSection from "./components/HeroSection.vue";
+
 export default {
   name: "App",
-  components: { Navbar },
+  components: { Navbar, HeroSection },
 };
 </script>
 
