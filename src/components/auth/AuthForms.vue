b3dfbc6e83ccbb40fc2702712f6a9b0994494ff2
Yavor Spassov <ygspasov@gmail.com>
2023-07-27 16:40:11 +0300

    Adding styles to validator messages

--- src/components/auth/AuthForms.vue
+++ src/components/auth/AuthForms.vue
@@ -15,7 +15,11 @@
           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="name@gmail.com"
         />
-        <p class="mt-2" v-for="error of v$.l_email.$errors" :key="error.$uid">
+        <p
+          class="mt-2 text-sm text-red-600 dark:text-red-500"
+          v-for="error of v$.l_email.$errors"
+          :key="error.$uid"
+        >
           <strong>{{ error.$message }}</strong>
         </p>
       </div>
@@ -30,7 +34,11 @@
           @blur="v$.l_password.$touch"
           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         />
-        <p class="mt-2" v-for="error of v$.l_password.$errors" :key="error.$uid">
+        <p
+          class="mt-2 text-sm text-red-600 dark:text-red-500"
+          v-for="error of v$.l_password.$errors"
+          :key="error.$uid"
+        >
           <strong>{{ error.$message }}</strong>
         </p>
       </div>
