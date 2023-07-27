5571790c7f3ba695060f608920aa46dd33f0ceb7
Yavor Spassov <ygspasov@gmail.com>
2023-07-27 05:02:04 +0300

    Finishing login form's validation

--- src/components/auth/AuthModal.vue
+++ src/components/auth/AuthModal.vue
@@ -42,16 +42,20 @@
           </div>
           <!-- Modal body -->
           <div class="p-6">
-            <AuthForms :loginOrRegister="loginOrRegister" />
+            <AuthForms
+              :loginOrRegister="loginOrRegister"
+              @l_valid="(valid) => (this.l_valid = valid)"
+            />
           </div>
           <!-- Modal footer -->
           <div
             class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600"
           >
             <button
+              :disabled="!l_valid"
               data-modal-hide="defaultModal"
               type="button"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:opacity-60"
             >
               Submit
             </button>
@@ -74,6 +78,7 @@ export default {
   data() {
     return {
       loginOrRegister: true,
+      l_valid: false,
     };
   },
   components: {
@@ -87,6 +92,11 @@ export default {
     register() {
       console.log("register");
       this.loginOrRegister = false;
+    },
+    activateSubmit(valid) {
+      console.log("activateSubmit");
+      console.log("valid", valid);
+      this.l_valid = valid;
     },
   },
 };
