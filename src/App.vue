b4bcb2a881f5c1df30810911f50fd09e359f962e
Yavor Spassov <ygspasov@gmail.com>
2023-07-31 01:07:57 +0300

    Login the user to Firebase

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,27 @@
 import Navbar from "./components/NavBar.vue";
 import HeroSection from "./components/HeroSection.vue";
 import SongsList from "./components/SongsList.vue";
+import { mapState } from "pinia";
+import { authStore } from "@/stores/authStore";
+import "./firebase/firebase";
+import { getAuth } from "firebase/auth";
+const auth = getAuth();
 
 export default {
   name: "App",
+  data() {
+    return {};
+  },
   components: { Navbar, HeroSection, SongsList },
+  computed: {
+    ...mapState(authStore, ["userLoggedIn"]),
+  },
+  created() {
+    console.log("auth.currentUser", auth.currentUser);
+    if (auth.currentUser) {
+      this.userLoggedIn = true;
+    }
+  },
 };
 </script>
 
--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -1,4 +1,7 @@
 import { defineStore } from "pinia";
+import "@/firebase/firebase";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+const auth = getAuth();
 
 export const authStore = defineStore("auth", {
   state: () => ({
@@ -7,6 +10,22 @@ export const authStore = defineStore("auth", {
   actions: {
     loginUser() {
       this.userLoggedIn = true;
+    },
+    async authenticate(email, password) {
+      await signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          console.log("successful login");
+          console.log("userCredential", userCredential);
+          this.userLoggedIn = true;
+          // Signed in
+          // const user = userCredential.user;
+          // ...
+        })
+        .catch((error) => {
+          console.log("error", error.message);
+          // const errorCode = error.code;
+          // const errorMessage = error.message;
+        });
     },
   },
 });
