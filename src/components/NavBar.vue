635c57e5111f9035ecf429a7e0e7300bfb4ade37
Yavor Spassov <ygspasov@gmail.com>
2023-08-02 04:03:16 +0300

    Adding the ManageSongs design

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,9 +4,11 @@
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" class="flex items-center">
           <img src="@/assets/Music.png" class="h-8 mr-3" alt="Music app Logo" />
-          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+          <router-link
+            to="/"
+            class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
             >FlowMusic
-          </span>
+          </router-link>
         </a>
         <button
           data-collapse-toggle="navbar-multi-level"
@@ -37,19 +39,19 @@
             class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
           >
             <li>
-              <a
-                href="#"
+              <router-link
+                to="/"
                 class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
                 aria-current="page"
-                >Home</a
+                >Home</router-link
               >
             </li>
 
             <li v-if="userLoggedIn">
-              <a
-                href="#"
+              <router-link
+                to="/manage"
                 class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >Manage</a
+                >Manage</router-link
               >
             </li>
 
