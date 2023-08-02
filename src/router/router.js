0dfb89376ae3adffdcaeb7e9698a703c4141e6ed
Yavor Spassov <ygspasov@gmail.com>
2023-08-02 04:14:42 +0300

    Fixing ManageSongs's routing

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import SongsList from "@/components/SongsList.vue";
+import ManageSongs from "@/components/ManageSongs.vue";
 export const router = createRouter({
   history: createWebHistory(),
-  routes: [{ path: "/", name: "Home", component: SongsList }],
+  routes: [
+    { path: "/", name: "Home", component: SongsList },
+    { path: "/manage", name: "Manage", component: ManageSongs },
+  ],
 });
