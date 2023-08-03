c9ab409309fb3f0de8f6303460a4fca17ae69f57
Yavor Spassov <ygspasov@gmail.com>
2023-08-03 04:22:15 +0300

    Adding the 404 page - part2

---
+++ src/components/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="flex flex-col md:flex-row items-center md:items-start justify-start md:justify-center text-red-600"
+  >
+    <h1 class="">We can't find the page you are looking for.</h1>
+    <span class="">This page has been relocated or removed.</span>
+  </div>
+  <div class="mt-5 text-center">
+    <router-link
+      to="/"
+      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      >Go Home</router-link
+    >
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped></style>
