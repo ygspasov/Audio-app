07e843c062727fee31beb4e9bcf3b4ac236ba100
Yavor Spassov <yavor@hph.io>
2023-08-03 04:21:53 +0300

    Adding the 404 page

--- src/router/router.js
+++ src/router/router.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 import SongsList from "@/components/SongsList.vue";
 import ManageSongs from "@/components/ManageSongs.vue";
+import NotFound from "@/components/NotFound.vue";
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", name: "Home", component: SongsList },
     { path: "/manage", name: "Manage", component: ManageSongs },
+    { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
   ],
   linkExactActiveClass: "text-sky-700",
 });
