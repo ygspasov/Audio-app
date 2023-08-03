d094034e6a7d4624d08026157a08c20d2e3704a5
Yavor Spassov <yavor@hph.io>
2023-08-03 07:18:34 +0300

    Adding navigation guards to /manage and redirecting the user to the home page after log out

--- src/router/router.js
+++ src/router/router.js
@@ -2,12 +2,18 @@ import { createRouter, createWebHistory } from "vue-router";
 import SongsList from "@/components/SongsList.vue";
 import ManageSongs from "@/components/ManageSongs.vue";
 import NotFound from "@/components/NotFound.vue";
-export const router = createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", name: "Home", component: SongsList },
-    { path: "/manage", name: "Manage", component: ManageSongs },
+    {
+      path: "/manage",
+      name: "Manage",
+      component: ManageSongs,
+    },
     { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
   ],
   linkExactActiveClass: "text-sky-700",
 });
+
+export { router };
