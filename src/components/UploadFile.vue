33e3344c746433b408ef30d8f0ba1b5f5a675e5a
Yavor Spassov <yavor@hph.io>
2023-08-04 07:24:27 +0300

    Preparing file upload

--- src/components/UploadFile.vue
+++ src/components/UploadFile.vue
@@ -1,12 +1,21 @@
 <template>
   <div
     class="basis-1/4 mb-4 md:mb-0 mx-1 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+    :class="{ 'bg-gray-300 border-gray-400 border-solid': isDraggedOver }"
+    @drag.prevent.stop=""
+    @dragstart.prevent.stop=""
+    @dragend.prevent.stop="isDraggedOver = false"
+    @dragover.prevent.stop="isDraggedOver = true"
+    @dragenter.prevent.stop="isDraggedOver = true"
+    @dragleave.prevent.stop="isDraggedOver = false"
+    @drop.prevent.stop="uploadFiles($event)"
   >
     <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Upload</h5>
     <div class="flex items-center justify-center w-full">
       <label
         for="dropzone-file"
         class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+        :class="{ 'bg-gray-300 border-gray-400 border-solid': isDraggedOver }"
       >
         <div class="flex flex-col items-center justify-center pt-5 pb-6">
           <svg
@@ -43,6 +52,19 @@
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      isDraggedOver: false,
+    };
+  },
+  methods: {
+    uploadFiles(event) {
+      this.isDraggedOver = false;
+      const files = [...event.dataTransfer.files];
+      console.log("files", files);
+    },
+  },
+};
 </script>
 <style></style>
