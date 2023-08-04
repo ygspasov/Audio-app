f4ff531fb44f3ff363ef0af3898ae55402efc9e7
Yavor Spassov <ygspasov@gmail.com>
2023-08-04 05:18:29 +0300

    Moving the file upload into a separate component

--- src/components/ManageSongs.vue
+++ src/components/ManageSongs.vue
@@ -1,51 +1,9 @@
 <template>
-  <!-- Upload -->
   <div id="manageSongs" class="max-w-screen-2lg mx-auto">
     <div
       class="h-screen flex flex-col md:flex-row items-center md:items-start justify-start md:justify-center"
     >
-      <div
-        class="basis-1/4 mb-4 md:mb-0 mx-1 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-      >
-        <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Upload</h5>
-        <div class="flex items-center justify-center w-full">
-          <label
-            for="dropzone-file"
-            class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-          >
-            <div class="flex flex-col items-center justify-center pt-5 pb-6">
-              <svg
-                class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 16"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                />
-              </svg>
-              <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                <span class="font-semibold">Click to upload</span> or drag and drop
-              </p>
-              <p class="text-xs text-gray-500 dark:text-gray-400">
-                SVG, PNG, JPG or GIF (MAX. 800x400px)
-              </p>
-            </div>
-            <input id="dropzone-file" type="file" class="hidden" />
-          </label>
-        </div>
-        <div class="my-1 text-base font-medium text-blue-700 dark:text-blue-500">
-          Gamma Ray - Pale Rider
-        </div>
-        <div class="w-full bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-700">
-          <div class="bg-blue-600 h-2.5 rounded-full" style="width: 45%"></div>
-        </div>
-      </div>
+      <UploadFile />
       <!-- Songs -->
 
       <div
@@ -113,8 +71,12 @@
 </template>
 <script>
 import { authStore } from "@/stores/authStore";
+import UploadFile from "./UploadFile.vue";
 export default {
   name: "ManageSongs",
+  components: {
+    UploadFile,
+  },
   beforeRouteEnter(to, from, next) {
     const auth = authStore();
     if (auth.userLoggedIn) {
