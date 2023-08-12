3631c85c92cc82793d3ad8b3c1f49e9ccd94f217
Yavor Spassov <yavor@hph.io>
2023-08-12 21:01:20 +0300

    Moving the song's edit form to its modal

--- src/components/EditModal.vue
+++ src/components/EditModal.vue
@@ -33,38 +33,50 @@
           <span class="sr-only">Close modal</span>
         </button>
         <div class="p-6 text-center">
-          <svg
-            class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 20"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-            />
-          </svg>
-          <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-            Are you sure you want to delete this product?
-          </h3>
-          <button
-            data-modal-hide="popup-modal"
-            type="button"
-            class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
-          >
-            Yes, I'm sure
-          </button>
-          <button
-            data-modal-hide="popup-modal"
-            type="button"
-            class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
-          >
-            No, cancel
-          </button>
+          <h3 class="text-xl">Song Name</h3>
+          <form class="mt-2">
+            <div class="mb-6">
+              <label
+                for="songName"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >Song Title</label
+              >
+              <input
+                type="songName"
+                id="songName"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Iron Maiden - Seventh son of the seventh son"
+                required
+              />
+            </div>
+            <div class="mb-6">
+              <label
+                for="genre"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >Genre</label
+              >
+              <input
+                type="genre"
+                id="genre"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Heavy metal"
+                required
+              />
+            </div>
+            <div class="flex flex-col md:flex-row">
+              <button
+                type="submit"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Submit
+              </button>
+              <button
+                class="my-2 md:my-0 ml-0 md:ml-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Go Back
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
