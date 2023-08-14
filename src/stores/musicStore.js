be35d952435b9afe368e52532ad57a159a4ab85c
Yavor Spassov <ygspasov@gmail.com>
2023-08-14 19:19:54 +0300

    Setting up songs editing

--- src/stores/musicStore.js
+++ src/stores/musicStore.js
@@ -3,10 +3,14 @@ import { defineStore } from "pinia";
 export const musicStore = defineStore("music", {
   state: () => ({
     songsLoading: false,
+    songId: "",
   }),
   actions: {
     loadSongs(val) {
       val === "yes" ? (this.songsLoading = true) : (this.songsLoading = false);
+    },
+    setSongId(val) {
+      this.songId = val;
     },
   },
 });
