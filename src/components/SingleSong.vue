fb34352ba87aeaf9f12c5e24b771af54f1220e7b
Yavor Spassov <yavor@hph.io>
2023-08-20 02:47:44 +0300

    Preparing the SingleSong component and adding its routing

---
+++ src/components/SingleSong.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1 class="text-2xl text-center">Single Song {{ $route.params.id }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "SingleSong",
+};
+</script>
+<style></style>
